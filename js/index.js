cd62b42da96495b7c1d7019eef944c1ad0bcce74
LAPTOP-AD6TEQ1R\Admin <ngotrieuhao@gmail.com>
2022-08-15 16:53:45 +0700

    update mousemove on mobile

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
 var balls = document.getElementsByClassName("eye__ball");
-document.onmousemove = function (event) {
-  var x = (event.clientX * 100) / window.innerWidth + "%";
-  var y = (event.clientY * 100) / window.innerHeight + "%";
+
+document.addEventListener("mousemove", (e) => {
+  var x = (e.clientX * 100) / window.innerWidth + "%";
+  var y = (e.clientY * 100) / window.innerHeight + "%";
 
   for (var i = 0; i < 2; i++) {
     balls[i].style.left = x;
     balls[i].style.top = y;
     balls[i].style.transform = "translate(-" + x + ", -" + y + ")";
   }
-};
+});
+
+document.addEventListener("touchmove", (e) => {
+  var x = (e.clientX * 100) / window.innerWidth + "%";
+  var y = (e.clientY * 100) / window.innerHeight + "%";
+
+  for (var i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].style.transform = "translate(-" + x + ", -" + y + ")";
+  }
+});
